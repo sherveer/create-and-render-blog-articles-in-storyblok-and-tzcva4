6c7b052d1ebd6c5bab976762e1b0404bd28812b9
sherveer <37408974+sherveer@users.noreply.github.com>
2022-11-01 10:49:14 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# create-and-render-blog-articles-in-storyblok-and-tzcva4
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/create-and-render-blog-articles-in-storyblok-and-tzcva4)
---
+++ pages/index.js
@@ -0,0 +1,46 @@
+import Head from 'next/head';
+
+import {
+  useStoryblokState,
+  getStoryblokApi,
+  StoryblokComponent,
+} from '@storyblok/react';
+import Layout from '../components/Layout';
+
+export default function Home({ story }) {
+  story = useStoryblokState(story, {
+    resolveRelations: ['popular-articles.articles'],
+  });
+
+  return (
+    <div>
+      <Head>
+        <title>Home</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Layout>
+        <StoryblokComponent blok={story.content} />
+      </Layout>
+    </div>
+  );
+}
+
+export async function getStaticProps() {
+  let slug = 'home';
+
+  let sbParams = {
+    version: 'draft', // or 'published'
+    resolve_relations: ['popular-articles.articles'],
+  };
+
+  const storyblokApi = getStoryblokApi();
+  let { data } = await storyblokApi.get(`cdn/stories/${slug}`, sbParams);
+
+  return {
+    props: {
+      story: data ? data.story : false,
+      key: data ? data.story.id : false,
+    },
+    revalidate: 3600,
+  };
+}
